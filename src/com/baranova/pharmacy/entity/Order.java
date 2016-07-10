e8e53e68712a4fc20dc850793d76972982a45da2
katerinabaranova <katebaranova@me.com>
2016-07-10 16:29:50 +0300

    order class

--- src/com/baranova/pharmacy/entity/Order.java
+++ src/com/baranova/pharmacy/entity/Order.java
@@ -1,7 +1,38 @@
 package com.baranova.pharmacy.entity;
 
-/**
- * Created by Ekaterina on 7/10/16.
- */
-public class Order {
+public class Order extends Entity {
+
+    private long fkUserID;
+    private long fkMedicineID;
+    private int quantity;
+    private int totalAmount;
+    private boolean paid;
+    private boolean delivery;
+
+    public Order(){
+    }
+    public Order(long id, long fkUserID, long fkMedicineID, int quantity, int totalAmount,
+                 boolean paid, boolean delivery) {
+        super(id);
+        this.fkUserID = fkUserID;
+        this.fkMedicineID = fkMedicineID;
+        this.quantity = quantity;
+        this.totalAmount = totalAmount;
+        this.paid = paid;
+        this.delivery = delivery;
+    }
+
+    public long getFkUserID() {return fkUserID;}
+    public long getFkMedicineID() {return fkMedicineID;}
+    public int getQuantity() {return quantity;}
+    public int getTotalAmount() {return totalAmount;}
+    public boolean isPaid() {return paid;}
+    public boolean isDelivery() {return delivery;}
+
+    public void setFkUserID(long fkUserID) {this.fkUserID = fkUserID;}
+    public void setFkMedicineID(long fkMedicineID) {this.fkMedicineID = fkMedicineID;}
+    public void setQuantity(int quantity) {this.quantity = quantity;}
+    public void setTotalAmount(int totalAmount) {this.totalAmount = totalAmount;}
+    public void setPaid(boolean paid) {this.paid = paid;}
+    public void setDelivery(boolean delivery) {this.delivery = delivery;}
 }
